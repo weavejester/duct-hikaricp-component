e8f2913cfee2c30c4cf756b8f2bd5c0f3522375e
James Reeves <jreeves@weavejester.com>
2014-12-18 15:13:01 +0000

    Add basic HikariCP component and deps

--- project.clj
+++ project.clj
@@ -3,4 +3,9 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]])
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [com.stuartsierra/component "0.2.2"]
+                 [com.zaxxer/HikariCP "2.2.5"]]
+  :profiles
+  {:dev {:dependencies [[org.clojure/java.jdbc "0.3.6"]
+                        [com.h2database/h2 "1.4.183"]]}})
--- src/duct/component/hikaricp.clj
+++ src/duct/component/hikaricp.clj
@@ -1,2 +1,19 @@
-(ns duct.component.hikaricp)
+(ns duct.component.hikaricp
+  (:require [com.stuartsierra.component :as component])
+  (:import [com.zaxxer.hikari HikariDataSource]))
 
+(defn- make-datasource [{:keys [uri]}]
+  (doto (HikariDataSource.)
+    (.setJdbcUrl uri)))
+
+(defrecord HikariCP [uri]
+  component/Lifecycle
+  (start [component]
+    (if (:spec component)
+      component
+      (assoc component :spec {:datasource (make-datasource component)})))
+  (stop [component]
+    (if-let [spec (:spec component)]
+      (do (.close (:datasource component))
+          (dissoc component :spec))
+      component)))
