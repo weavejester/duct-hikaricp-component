f95405cd8a369f04a500757d73dc1f1acda73759
James Reeves <jreeves@weavejester.com>
2014-12-19 14:46:52 +0000

    Add common options to component

--- src/duct/component/hikaricp.clj
+++ src/duct/component/hikaricp.clj
@@ -1,17 +1,32 @@
 (ns duct.component.hikaricp
   (:require [com.stuartsierra.component :as component])
-  (:import [com.zaxxer.hikari HikariDataSource]))
+  (:import [com.zaxxer.hikari HikariConfig HikariDataSource]))
 
-(defn- make-datasource [{:keys [uri]}]
-  (doto (HikariDataSource.)
-    (.setJdbcUrl uri)))
+(defn- make-config
+  [{:keys [uri username password auto-commit? conn-timeout idle-timeout
+           max-lifetime conn-test-query min-idle max-pool-size pool-name]}]
+  (let [cfg (HikariConfig.)]
+    (when uri                  (.setJdbcUrl cfg uri))
+    (when username             (.setUsername cfg username))
+    (when password             (.setPassword cfg password))
+    (when (some? auto-commit?) (.setAutoCommit cfg auto-commit?))
+    (when conn-timeout         (.setConnectionTimeout cfg conn-timeout))
+    (when idle-timeout         (.setIdleTimeout cfg conn-timeout))
+    (when max-lifetime         (.setMaxLifetime cfg max-lifetime))
+    (when max-pool-size        (.setMaximumPoolSize cfg max-pool-size))
+    (when min-idle             (.setMinimumIdle cfg min-idle))
+    (when pool-name            (.setPoolName cfg pool-name))
+    cfg))
+
+(defn- make-spec [component]
+  {:datasource (HikariDataSource. (make-config component))})
 
 (defrecord HikariCP [uri]
   component/Lifecycle
   (start [component]
     (if (:spec component)
       component
-      (assoc component :spec {:datasource (make-datasource component)})))
+      (assoc component :spec (make-spec component))))
   (stop [component]
     (if-let [spec (:spec component)]
       (do (.close (:datasource component))
