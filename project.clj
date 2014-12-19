b26284b862f65022cf9e2bd65f3dae43ed0e6848
James Reeves <jreeves@weavejester.com>
2014-12-19 17:44:12 +0000

    Change HikariCP dependency to Java 6 version

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [com.stuartsierra/component "0.2.2"]
-                 [com.zaxxer/HikariCP "2.2.5"]]
+                 [com.zaxxer/HikariCP-java6 "2.2.5"]]
   :profiles
   {:dev {:dependencies [[org.clojure/java.jdbc "0.3.6"]
                         [com.h2database/h2 "1.4.183"]]}})
